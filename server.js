4ce94ffab4b82b0e90ad63b4e37ef9e07956babd
Lucas Barbosa <barbosalucasroberto@gmail.com>
2021-09-01 23:20:51 -0300

    Configuracion heroku agregada

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(__dirname + "/angularapp"));
+
+app.listen(process.env.PORT || 8080);
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/angularapp/index.html"));
+});
+console.log("Build done");
