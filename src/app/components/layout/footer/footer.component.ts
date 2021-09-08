15162651214816b5ec6f85349dfa768de81358a5
Lucas Barbosa <barbosalucasroberto@gmail.com>
2021-09-08 11:42:35 -0300

    layout retocado final responsive

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  currentDate!: Date;
+  constructor() {}
 
   ngOnInit(): void {
+    this.currentDate = new Date();
   }
-
 }
