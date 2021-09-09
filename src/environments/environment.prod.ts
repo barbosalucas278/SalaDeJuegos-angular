24e531d9047cbd49fc568d70f3da4a7f9b4f2745
Lucas Barbosa <barbosalucasroberto@gmail.com>
2021-09-09 18:43:24 -0300

    agregadas las variables de entorno en produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAImverduNhWyuXVG75jtTusU9gPGtlcQw',
+    authDomain: 'lbarbosa-tp-lb4-28ecf.firebaseapp.com',
+    databaseURL: 'https://lbarbosa-tp-lb4-28ecf-default-rtdb.firebaseio.com',
+    projectId: 'lbarbosa-tp-lb4-28ecf',
+    storageBucket: 'lbarbosa-tp-lb4-28ecf.appspot.com',
+    messagingSenderId: '703073583820',
+    appId: '1:703073583820:web:7daff820b53d7b30753869',
+  },
 };
